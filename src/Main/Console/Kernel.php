8c4eb479eb14ee19b07399a02e402b6aaf6ea47c
Dmitriy <stdimensiy@yandex.ru>
2023-01-12 01:14:31 +0300

    Расширение функционала фреймворка  - Корректировка ядра консоли

--- src/Main/Console/Kernel.php
+++ src/Main/Console/Kernel.php
@@ -2,8 +2,23 @@
 
 namespace Stub\Framework\Main\Console;
 
+use Stub\Framework\Contracts\Main\Application;
+
 class Kernel implements \Stub\Framework\Contracts\Console\Kernel
 {
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+        self::hi();
+    }
 
     public function hi()
     {
