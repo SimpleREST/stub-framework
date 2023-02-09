20423e49b9e4b6c7fcb5f61993cdb5d66aa7f511
Dmitriy <stdimensiy@yandex.ru>
2023-02-09 23:05:57 +0300

    Работа с ресурсами - полная переработка механизма работы со строковыми ресурсами для вью

--- src/Http/View/Stub.php
+++ src/Http/View/Stub.php
@@ -70,7 +70,7 @@ class Stub implements View
                                 </h1>
                             </div>
                             <div class=\"contact-link float-right\">
-                                <a href=\"#contacts\" class=\"scrollto\">{$app->get('contacts_link_text', 'administrator contacts')}</a>
+                                <a href=\"#contacts\" class=\"scrollto\">" . R::$contacts_link_text . "</a>
                             </div>
                         </div>
                     </header>
@@ -79,14 +79,14 @@ class Stub implements View
                     <div class=\"container-fluid\" style=\"padding-right: 0; padding-left: 0;\">
                     <!-- ======= Base Stub Section ======= -->
                     <section id=\"stub\"
-                             style=\"background-image: url('img/{$app->get('base_background', 'simpleplug-base-bg.jpg')}')\">
+                             style=\"background-image: url('img/" . R::$base_background . "')\">
                         <div class=\"stub-container\">
                             <BR><BR>
-                            <h1>{$app->get('base_title', 'We Build Simple REST')}</h1>
-                            <h2>{$app->get('base_note', 'SimpleStub / We Build')}</h2>
+                            <h1>" . R::$base_title . "</h1>
+                            <h2>" . R::$base_note . "</h2>
                             <div id=\"countdown\" class=\"countdown-timer\"
-                                 data-deadline=\"{$app->get('countdown_deadline', '2025/12/30')}\"
-                                 data-pattern=\"{$app->get('countdown_pattern', '%w w %d d %H:%M:%S')}\"></div>
+                                 data-deadline=\"" . R::$countdown_deadline . "\"
+                                 data-pattern=\"" . R::$countdown_pattern . "\"></div>
                         </div>
                     </section><!-- End Base Stub section -->
                     </div>
@@ -97,7 +97,7 @@ class Stub implements View
                             <div class=\"container\">
                     
                                 <div class=\"section-title\">
-                                    <h2>{$app->get('contacts_section_title_text', 'Contacts')}</h2>
+                                    <h2>" . R::$contacts_section_title_text . "</h2>
                                 </div>
                     
                                 <div class=\"row contact-info\">
@@ -105,18 +105,18 @@ class Stub implements View
                                     <div class=\"col-md-4\">
                                         <div class=\"contact-address\">
                                             <i class=\"icofont-google-map\"></i>
-                                            <h3>{$app->get('contacts_section_address_title_text', 'address')}</h3>
-                                            <address>{$app->get('contacts_section_address_content_text', '___')}</address>
+                                            <h3>" . R::$contacts_section_address_title_text . "</h3>
+                                            <address>" . R::$contacts_section_address_content_text . "</address>
                                         </div>
                                     </div>
                     
                                     <div class=\"col-md-4\">
                                         <div class=\"contact-phone\">
                                             <i class=\"icofont-phone\"></i>
-                                            <h3>{$app->get('contact_section_phone_title_text', 'Phone')}</h3>
+                                            <h3>" . R::$contacts_section_phone_title_text . "</h3>
                                             <p>
-                                                <a href=\"tel:{$app->get('contact_phone_to_script', '')}\">
-                                                    {$app->get('contact-phone_to_display', '+_ (___) ___-__-__')}</a>
+                                                <a href=\"tel:" . R::$contact_phone_to_script . "\">
+                                                    " . R::$contact_phone_to_display . "</a>
                                             </p>
                                         </div>
                                     </div>
@@ -124,10 +124,10 @@ class Stub implements View
                                     <div class=\" col-md-4\">
                                         <div class=\"contact-email\">
                                             <i class=\"icofont-envelope\"></i>
-                                            <h3>{$app->get('contacts_section_email_title_text', 'Email')}</h3>
+                                            <h3>" . R::$contacts_section_email_title_text . "</h3>
                                             <p>
-                                                <a href=\"mailto: {$app->get('contact-email_to_script', '')}\">
-                                                    {$app->get('contact-email_to_display', '')}
+                                                <a href=\"mailto: " . R::$contact_email_to_script . "\">
+                                                    " . R::$contact_email_to_display . "
                                                 </a>
                                             </p>
                                         </div>
@@ -143,7 +143,7 @@ class Stub implements View
                     <footer id=\"footer\">
                         <div class=\"container\">
                             <div class=\"copyright\">
-                                &copy; Copyright <strong><span>{$app->get('http', 'simplerest.ru')}</span></strong>
+                                &copy; Copyright <strong><span>" . R::$http . "</span></strong>
                                 All Rights Reserved
                             </div>
                             <div class=\"credits\">
@@ -163,7 +163,7 @@ class Stub implements View
     {
         return sprintf(/** @lang text */ "%s<html lang=\"%s\">%s<body>%s%s</body></html>",
             $this->docType,
-            $this->app->get('base_lang', 'en'),
+            R::$base_lang,
             $this->head,
             $this->body,
             $this->endHtmlScripts);
--- src/Main/Assets/Resource.php
+++ src/Main/Assets/Resource.php
@@ -17,7 +17,7 @@ class Resource
     public static $keywords = "SimpleStub";
     public static $domain = "SimpleStub";
     public static $contacts_link_text = "administrator contacts";
-    public static $base_background = "administrator contacts";
+    public static $base_background = "simpleplug-base-bg.jpg";
     public static $base_title = "We Build Simple REST";
     public static $base_note = "SimpleStub / We Build";
     public static $countdown_deadline = "2025/12/30";
@@ -25,13 +25,14 @@ class Resource
     public static $contacts_section_title_text = "Contacts";
     public static $contacts_section_address_title_text = "address";
     public static $contacts_section_address_content_text = "___";
-    public static $contact_section_phone_title_text = "Phone";
+    public static $contacts_section_phone_title_text = "Phone";
     public static $contact_phone_to_script = "";
     public static $contact_phone_to_display = "+_ (___) ___-__-__";
     public static $contacts_section_email_title_text = "Email";
     public static $contact_email_to_script = "";
     public static $contact_email_to_display = "";
     public static $http = "simplerest.ru";
+    public static $base_lang = "en";
 
     private function __construct()
     {
